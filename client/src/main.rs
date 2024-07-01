041c9a09219b0935f249d43564bc76c0df4f3b56
XX <freecoder.xx@gmail.com>
2024-07-01 17:23:48 +0300

    fix: use action instead resource

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,43 +1,43 @@
-use std::{cell::Cell, collections::VecDeque};
+use std::cell::Cell;
+use std::collections::VecDeque;
 
 use gloo_net::http::Request;
+use leptos::ev::SubmitEvent;
+use leptos::html::Input;
 use leptos::{
-    component, create_effect, create_node_ref, create_resource, create_signal, ev::SubmitEvent,
-    html::Input, logging, view, For, IntoView, NodeRef, SignalGet, SignalSet, SignalUpdate,
+    component, create_action, create_effect, create_node_ref, create_signal, logging, view, For, IntoView, NodeRef,
+    SignalGet, SignalUpdate,
 };
 
 #[component]
 fn App() -> impl IntoView {
-    let (request, set_request) = create_signal(None);
     let (responses, set_responses) = create_signal(VecDeque::new());
 
     // Load the response when a request message spawns
-    let load_response = create_resource(
-        move || request.get(),
-        |maybe_msg| async move {
-            if let Some(msg) = maybe_msg {
-                let response_result = Request::get(&format!("/versus/{msg}")).send().await;
-                let text = match response_result {
-                    Ok(response) => response
-                        .text()
-                        .await
-                        .unwrap_or_else(|err| format!("Response error: {err}")),
-                    Err(err) => format!("Request error: {err}"),
-                };
+    let send_request = create_action(|msg: &String| {
+        let url = format!("/versus/{msg}");
+        let request = Request::get(&url);
+        logging::log!("Request: {url}");
 
-                logging::log!("Response: {text}");
-                Some(text)
-            } else {
-                None
-            }
-        },
-    );
+        async move {
+            let text = match request.send().await {
+                Ok(response) => response
+                    .text()
+                    .await
+                    .unwrap_or_else(|err| format!("Response error: {err}")),
+                Err(err) => format!("Request error: {err}"),
+            };
+
+            logging::log!("Response: {text}");
+            text
+        }
+    });
+    let load_response = send_request.value();
 
     // Update the responses list when we get a response
     let id_counter = Cell::new(1_usize);
     create_effect(move |_| {
-        let maybe_response = load_response.get();
-        if let Some(Some(response)) = maybe_response {
+        if let Some(response) = load_response.get() {
             let id = id_counter.replace(id_counter.get() + 1);
 
             set_responses.update(move |responses| {
@@ -52,8 +52,8 @@ fn App() -> impl IntoView {
         // stop the page from reloading!
         ev.prevent_default();
 
-        let value = input_element.get().expect("<input> must exist").value();
-        set_request.set(Some(value));
+        let input = input_element.get().expect("<input> must exist");
+        send_request.dispatch(input.value());
     };
 
     view! {
@@ -83,5 +83,5 @@ fn App() -> impl IntoView {
 fn main() {
     console_error_panic_hook::set_once();
 
-    leptos::mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(App)
 }
